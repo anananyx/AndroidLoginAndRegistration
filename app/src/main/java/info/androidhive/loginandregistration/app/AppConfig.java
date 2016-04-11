714cf244f4367c00b3ad3ace54fb19c79a2b72ad
xiao yue <xiao.yue13@student.xjtlu.edu.cn>
2016-04-11 15:41:01 +0800

    Initial commit

---
+++ app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
@@ -0,0 +1,9 @@
+package info.androidhive.loginandregistration.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://192.168.0.102/android_login_api/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://10.8.27.55/~yuexiao/signin.php";
+}
